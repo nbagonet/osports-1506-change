ad9924356f62cd728640ce74e5364117633c7185
ShuoWang <nbagonet@gmail.com>
2015-07-14 23:15:54 +0800

    开发中

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-var as = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+var as = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
+  tobj = $('body');
 
 $(function() {
 
@@ -13,7 +14,7 @@ $(function() {
   var pw = new pageSwitch('wrap', {
     duration: 1000, //int 页面过渡时间
     direction: 1, //int 页面切换方向，0横向，1纵向
-    start: 1, //int 默认显示页面
+    start: 2, //int 默认显示页面
     loop: false, //bool 是否循环切换
     ease: 'ease', //string|function 过渡曲线动画，详见下方说明
     transition: 'scroll', //string|function转场方式，详见下方说明
@@ -48,6 +49,13 @@ $(function() {
     } else {
       p2.reset();
     }
+
+    // p3
+    if (cur == 3) {
+      p3.init();
+    } else {
+      p3.reset();
+    }
   });
 
 
@@ -60,37 +68,29 @@ $(function() {
   p1.init = function() {
     var _p1 = $('#p1');
 
-    // 冻结pw
-    // pw.freeze(true);
-
     // p1动画
-    // _p1.find('div').addClass('animated');
     _p1.find('.logo-ost1').removeClass('hide').addClass('zoomIn').one(as, function() {
       _p1.find('.txt1').removeClass('hide').addClass('fadeInUp').one(as, function() {
         _p1.find('.line').removeClass('hide').addClass('zoomIn').one(as, function() {
           _p1.find('.logo-t2').addClass('fadeOut');
           _p1.find('.txt1').addClass('stu2');
-          // _p1.find('.logo-t1').addClass('stu2').one(as, function() {
           _p1.find('.logo-t1').addClass('stu2');
-          $('body').oneTime('1s', function() {
+          tobj.oneTime('1s', function() {
             _p1.find('.logo-t3').removeClass('hide').addClass('fadeIn');
             _p1.find('.logo-t4').removeClass('hide').addClass('fadeIn').one(as, function() {
               _p1.find('.logo-t5').removeClass('hide').addClass('fadeInUp').one(as, function() {
                 _p1.find('.logo-t6').removeClass('hide').addClass('fadeInRight').one(as, function() {
                   _p1.find('.btn').removeClass('hide').addClass('fadeInDown').one(as, function() {
                     _p1.find('.btn').addClass('animate');
-                    _p1.find('.btn').on('click', function() {
+                    _p1.find('.btn').unbind('click').on('click', function() {
                       pw.pw.slide(1);
                     });
-                    // 解冻pw
-                    // pw.freeze(false);
                   });
                 });
               });
               _p1.find('.logo-t1').addClass('hide');
             });
           });
-          // });
         });
       });
     });
@@ -99,7 +99,9 @@ $(function() {
   // p1重置
   p1.reset = function() {
     var _p1 = $('#p1');
-    // _p1.find('div').removeClass('animated');
+    // 清除计时器
+    $('body').stopTime();
+    // 清除动画
     _p1.find('.logo-ost1,.line').addClass('hide').removeClass('zoomIn');
     _p1.find('.txt1,.logo-t5').addClass('hide').removeClass('fadeInUp');
     _p1.find('.logo-t2').removeClass('fadeOut');
@@ -115,13 +117,10 @@ $(function() {
    */
   var p2 = {};
 
-  // p1初始化
+  // p2初始化
   p2.init = function() {
     var p = $('#p2'),
       map = $('#map-10-china');
-
-    // 冻结pw
-    // pw.freeze(true);
 
     // 需要选中的省市数据
     p2MapData = ['BEJ', 'LIA', 'SHD', 'JSU', 'SHH', 'ZHJ', 'GUD', 'HKG', 'SCH', 'HUB', 'HEN', 'SHX'];
@@ -163,7 +162,7 @@ $(function() {
           // 点亮分社所在省市
           var mapObject = map.vectorMap('get', 'mapObject'),
             p2MapQue = function(time, regions) {
-              map.oneTime(time + 's', regions, function() {
+              tobj.oneTime(time + 's', regions, function() {
                 mapObject.setSelectedRegions(regions);
               });
             };
@@ -178,7 +177,7 @@ $(function() {
             refreshInterval: 5
           });
         });
-        $('.num2').oneTime('1s', 'p1Num2', function() {
+        tobj.oneTime('1s', 'p1Num2', function() {
           p.find('.num2 > .num, .num2 > .txt').removeClass('hide').addClass('fadeInUp').one(as, function() {
             // 摄影师数字自增
             p.find('.num2 > .plus').removeClass('hide').addClass('fadeIn');
@@ -193,8 +192,6 @@ $(function() {
             });
             // 摄影师icon动画
             p.find('.people > span').removeClass('hide').addClass('bounceInUp');
-            // 解冻pw
-            // pw.freeze(false);
           });
         });
       });
@@ -204,15 +201,18 @@ $(function() {
 
   // p2重置
   p2.reset = function() {
-    var p = $('#p2');
+    var p = $('#p2'),
+      map = $('#map-10-china');
 
     // 销毁中国地图
-    p.find('.map').empty();
+    map.empty();
+
+    // 清除计时器
+    $('body').stopTime();
 
     // 清除动画
-    // p.find('div,span').removeClass('animated');
     p.find('.tit').addClass('hide').removeClass('slideInLeft');
-    p.find('.map').addClass('hide').removeClass('zoomInDown');
+    map.addClass('hide').removeClass('zoomInDown');
     p.find('.num1 > .num,.num1>.txt').addClass('hide').removeClass('fadeInDown');
     p.find('.num1>.num,.num2>.num').empty();
     p.find('.num2 > .num, .num2 > .txt').addClass('hide').removeClass('fadeInUp');
@@ -220,9 +220,133 @@ $(function() {
     p.find('.num2 > .plus').addClass('hide').removeClass('fadeIn');
   };
 
+
+  /**
+   * p3
+   */
+  var p3 = {};
+
+  // p3初始化
+  p3.init = function() {
+    var p = $('#p3'),
+      map = $('#map-world');
+
+    // 需要选中的国家数据
+    p3MapData = ['JP', 'KR', 'MY', 'AT', 'US', 'CA', 'ES', 'GB', 'DE', 'FR', 'NL', 'CH', 'AR', 'TW', 'CN'];
+
+    // 如果已经存在世界地图，则需要先取消选中，但不需要重新初始化地图
+    if (map.html() != '') {
+      var mapObject = map.vectorMap('get', 'mapObject');
+      for (var i = 0; i < p3MapData.length; i++) {
+        mapObject.setSelectedRegions(p3MapData[i], false);
+      }
+    } else {
+      // 初始化世界地图
+      map.vectorMap({
+        map: 'world_mill_en',
+        backgroundColor: 'rgba(0,0,0,0)',
+        zoomOnScroll: false,
+        panOnDrag: false,
+        regionsSelectable: false,
+        regionStyle: {
+          initial: {
+            fill: '#88b8ad',
+            "fill-opacity": 1,
+            stroke: 'none',
+            "stroke-width": 0,
+            "stroke-opacity": 1
+          },
+          hover: {},
+          selected: {
+            fill: '#e35e5d'
+          },
+          selectedHover: {}
+        }
+      });
+    }
+
+    // 动效
+    p.find('.tit').removeClass('hide').addClass('fadeInRight').one(as, function() {
+      map.removeClass('hide').addClass('tinDownIn');
+      p.find('.num1>.num,.num1>.txt,.num2>.num,.num2>.txt').removeClass('hide').addClass('fadeInDown').one(as, function() {
+        // 点亮国家
+        var mapObject = map.vectorMap('get', 'mapObject'),
+          p3MapQue = function(time, regions) {
+            tobj.oneTime(time + 's', regions, function() {
+              mapObject.setSelectedRegions(regions);
+            });
+          };
+        for (var i = 1; i <= p3MapData.length; i++) {
+          p3MapQue(i / p3MapData.length, p3MapData[i - 1]);
+        }
+        // 国家及合作伙伴数字自增
+        p.find('.num1>.num').countTo({
+          from: 0,
+          to: 14,
+          speed: 1000,
+          refreshInterval: 5
+        });
+        p.find('.num2>.num').countTo({
+          from: 0,
+          to: 31,
+          speed: 1000,
+          refreshInterval: 5
+        });
+        // 显示图片数量自增
+        tobj.oneTime('1s', function() {
+          p.find('.num3>.num,.num3>.txt').removeClass('hide').addClass('fadeInDownBig');
+          p.find('.num3>.num').countTo({
+            from: 0,
+            to: 10000000,
+            speed: 2000,
+            refreshInterval: 5,
+            formatter: function(value, options) {
+              return (value.toFixed(0) + '').replace(/\d{1,3}(?=(\d{3})+(\.\d*)?$)/g, '$&,');
+            }
+          });
+        });
+        // 显示向下大括号
+        tobj.oneTime('4s', function() {
+          p.find('.arrow').removeClass('hide').addClass('animate').one(as, function() {
+            // 显示媒体数自增
+            p.find('.num4').removeClass('hide').addClass('fadeInDown');
+            p.find('.num4>.num').countTo({
+              from: 0,
+              to: 275,
+              speed: 1000,
+              refreshInterval: 5
+            });
+          });
+        });
+      });
+    });
+
+  };
+
+  // p3重置
+  p3.reset = function() {
+    var p = $('#p3'),
+      map = $('#map-world');
+
+    // 销毁世界地图
+    map.empty();
+
+    // 清除计时器
+    $('body').stopTime();
+
+    // 清除动画
+    p.find('.tit').addClass('hide').removeClass('fadeInRight');
+    map.addClass('hide').removeClass('tinDownIn');
+    p.find('.num1>.num,.num1>.txt,.num2>.num,.num2>.txt,.num4').addClass('hide').removeClass('fadeInDown');
+    p.find('.num1>.num,.num2>.num,.num3>.num,.num4>.num').empty();
+    p.find('.arrow').addClass('hide').removeClass('animate');
+    p.find('.num3>.num,.num3>.txt').addClass('hide').removeClass('fadeInDownBig');
+  };
+
+
   // 初始化p1
   Pace.on('hide', function() {
-    p2.init();
+    p3.init();
   });
 
 });
