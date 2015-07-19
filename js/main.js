d3e6f5e51806c50303fea6ca2a54504b5e774995
ShuoWang <nbagonet@gmail.com>
2015-07-19 18:54:46 +0800

    开发中

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ $(function() {
   var pw = new pageSwitch('wrap', {
     duration: 1000, //int 页面过渡时间
     direction: 1, //int 页面切换方向，0横向，1纵向
-    start: 6, //int 默认显示页面
+    start: 0, //int 默认显示页面
     loop: false, //bool 是否循环切换
     ease: 'ease', //string|function 过渡曲线动画，详见下方说明
     transition: 'scroll', //string|function转场方式，详见下方说明
@@ -389,8 +389,8 @@ $(function() {
     var p = $('#p4');
 
     p.find('.tit').removeClass('hide').addClass('fadeInDown').one(as, function() {
+      p.find('#partnerCloud').removeClass('hide');
       cloudInit();
-      p.find('#partnerCloud').removeClass('hide').fadeIn();
     });
 
     // 初始化logo云
@@ -441,7 +441,7 @@ $(function() {
 
     p.find('.tit').removeClass('hide').addClass('fadeInDown').one(as, function() {
       cloudInit();
-      p.find('#clientCloud').removeClass('hide').fadeIn();
+      p.find('#clientCloud').removeClass('hide');
     });
 
     // 初始化logo云
@@ -649,7 +649,7 @@ $(function() {
 
   // 初始化p1
   Pace.on('hide', function() {
-    p7.init();
+    p1.init();
   });
 
 });
